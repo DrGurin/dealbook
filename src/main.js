32703e522496ad0f750d8299415a2c1a4889140d
b.rogulia <b.rogulia@viseven.com>
2020-11-03 22:53:31 +0200

    add copy btn

--- src/main.js
+++ src/main.js
@@ -1,20 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import VueSlickCarousel from 'vue-slick-carousel'
-import '@fortawesome/fontawesome-free/css/all.css'
-import '@fortawesome/fontawesome-free/js/all.js'
-import {i18n} from "./plugins/i18n.js"
-import Chart from 'chart.js'
+import Vue from 'vue';
+import App from './App.vue';
+import VueSlickCarousel from 'vue-slick-carousel';
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/all.js';
+import {i18n} from "./plugins/i18n.js";
+import Chart from 'chart.js';
 
 Chart.defaults.global.defaultFontColor = 'transparent';
 Chart.defaults.global.legend.display = false;
 Chart.defaults.global.elements.line.tension = 0;
 
 
-Vue.config.productionTip = false
-Vue.component('VueSlickCarousel', VueSlickCarousel)
+Vue.config.productionTip = false;
+Vue.component('VueSlickCarousel', VueSlickCarousel);
 
 new Vue({
   i18n,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
