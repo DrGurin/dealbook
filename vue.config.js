86bc6365ca6536249356bc31daee165bf9b63f07
DrGurin <drgurin@MacBook-Pro-DrGurin.local>
2020-12-30 15:23:52 +0200

    update (vue.congif.js): add prerender into webpack

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
+var PrerenderSpaPlugin = require('prerender-spa-plugin');
+var path = require('path');
+
 module.exports = {
     publicPath: '',
     pages: {
@@ -5,5 +8,19 @@ module.exports = {
         entry: 'src/main.js',
         title: 'Deal Book'
       }
-    }
+    },
+    configureWebpack: config => {
+      console.log('Dealbook config: ', config);
+      if (process.env.NODE_ENV !== 'production') return
+      return {
+        plugins: [
+          new PrerenderSpaPlugin(
+            // Absolute path to compiled SPA
+            path.resolve(__dirname, 'dist'),
+            // List of routes to prerender
+            [ '/', '/about'],
+          ),
+        ]
+      }
+    },
   }
